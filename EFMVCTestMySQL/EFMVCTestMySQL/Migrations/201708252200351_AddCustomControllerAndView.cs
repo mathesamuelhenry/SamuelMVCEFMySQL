02fb2f7d52dc2efebf7dfe8d21813fed444e08b7
Mathe, Samuel <samuel.mathe@lexisnexisrisk.com>
2017-08-25 18:11:56 -0400

    seeding the database with values for membershiptypes #4

---
+++ EFMVCTestMySQL/EFMVCTestMySQL/Migrations/201708252200351_AddCustomControllerAndView.cs
@@ -0,0 +1,16 @@
+namespace EFMVCTestMySQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddCustomControllerAndView : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
