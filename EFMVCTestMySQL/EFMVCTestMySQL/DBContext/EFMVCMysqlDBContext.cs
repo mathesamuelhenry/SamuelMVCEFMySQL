de974db7c7b200238135f876f773f43709e54502
Mathe, Samuel <samuel.mathe@lexisnexisrisk.com>
2017-08-25 12:08:29 -0400

    Setup MYSQL Connection/DBContext #1

---
+++ EFMVCTestMySQL/EFMVCTestMySQL/DBContext/EFMVCMysqlDBContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using MySql.Data.Entity;
+using EFMVCTestMySQL.Models;
+
+namespace EFMVCTestMySQL.DBContext
+{
+    // Code-Based Configuration and Dependency resolution
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class EFMVCMysqlDBContext : DbContext
+    {
+        //Add your Dbsets here
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<MembershipType> MembershipTypes { get; set; }
+
+        public EFMVCMysqlDBContext()
+              
+           //Reference the name of your connection string
+            : base("TestSamuelMVCEFDB")
+              {
+        }
+    }
+}
