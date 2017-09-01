607aa1be9332955d15de125df194d3084f64090d
Mathe, Samuel <samuel.mathe@lexisnexisrisk.com>
2017-09-01 17:16:15 -0400

    changes to movie model

--- EFMVCTestMySQL/EFMVCTestMySQL/Models/Movie.cs
+++ EFMVCTestMySQL/EFMVCTestMySQL/Models/Movie.cs
@@ -9,13 +9,21 @@ namespace EFMVCTestMySQL.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
         [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+
         public DateTime DateAdded { get; set; }
+
         [Display(Name = "Number In Stock")]
         public int NumberInStock { get; set; }
+
         public Genre Genre { get; set; }
+
+        [Required]
         [Display(Name = "Genre")]
         public int GenreId { get; set; }
     }
