a2b93e8c410d1cb887a421f6bd093e749829e373
Mathe, Samuel <samuel.mathe@lexisnexisrisk.com>
2018-02-24 22:46:14 -0500

    WIP : intermediate commit for a package update

---
+++ EFMVCTestMySQL/EFMVCTestMySQL/Controllers/API/CustomersController.cs
@@ -0,0 +1,93 @@
+﻿using EFMVCTestMySQL.DBContext;
+using EFMVCTestMySQL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EFMVCTestMySQL.Controllers.API
+{
+    public class CustomersController : ApiController
+    {
+        private EFMVCMySqlDBContext _context;
+
+        public CustomersController()
+        {
+            _context = new EFMVCMySqlDBContext();
+        }
+
+        // GET /api/customers
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+        // GET /api/customers/1
+        public Customer GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return customer;
+        }
+
+        // POST /api/customers
+        [HttpPost]
+        public Customer CreateCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return customer;
+        }
+
+        // POST /api/customers
+        /*public Customer PostCustomer(Customer customer)
+        {
+
+        }
+        */
+
+        // PUT /api/customers/1
+        [HttpPut]
+        public Customer UpdateCustomer(int id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var customerInDB = _context.Customers.SingleOrDefault(c => c.Id == id);
+            
+            if (customerInDB == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            customerInDB.Name = customer.Name;
+            customerInDB.BirthDate = customer.BirthDate;
+            customerInDB.IsSubscribedToNewsLetter = customer.IsSubscribedToNewsLetter;
+            customerInDB.MembershipTypeId = customer.MembershipTypeId;
+
+            _context.SaveChanges();
+
+            return customer;
+        }
+
+        // DELETE /api/customers/1
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var customerInDB = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDB == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.Customers.Remove(customerInDB);
+            _context.SaveChanges();
+        }
+    }
+}
